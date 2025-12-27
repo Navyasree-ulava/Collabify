f2c62571b9fa51a394ecd8c736496fb5138d45a6
Navyasree <navya12345u@gmail.com>
2025-12-28 02:48:14 +0530

    db error fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,68 +1,82 @@
 import { Inngest } from "inngest";
 import prisma from "../configs/prisma.js";
 
-// Create a client to send and receive events
+// Create Inngest client
 export const inngest = new Inngest({ id: "project-management" });
 
-// Inngest Function to save user data in the database
+/**
+ * CREATE / SYNC USER
+ * Safe because user.created should only insert once
+ */
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
-    async ({ event }) => {
-        const { data } = event;
-        await prisma.user.create(
-            {
-                data: {
-                    id: data.id,
-                    email: data?.email_addresses[0]?.email_address,
-                    name: data?.first_name + " " + data?.last_name,
-                    image: data?.image_url,
-                }
-            }
-        )
-    }
-)
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
 
-// Inngest Function to delete user from database
+    await prisma.user.upsert({
+      where: { id: data.id },
+      update: {
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name ?? ""} ${data?.last_name ?? ""}`,
+        image: data?.image_url,
+      },
+      create: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name ?? ""} ${data?.last_name ?? ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+/**
+ * DELETE USER
+ * deleteMany is SAFE even if user doesn't exist
+ */
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user.deleted' },
-    async ({ event }) => {
-        const { data } = event;
-        await prisma.user.delete(
-            {
-                where: {
-                    id: data.id,
-                }
-            }
-        )
-    }
-)
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event;
 
-// Inngest Function to update user data in the database
+    await prisma.user.deleteMany({
+      where: { id: data.id },
+    });
+  }
+);
+
+/**
+ * UPDATE USER
+ * Use upsert to avoid "record not found" error
+ */
 const syncUserUpdation = inngest.createFunction(
-    { id: 'update-user-with-clerk' },
-    { event: 'clerk/user.updated' },
-    async ({ event }) => {
-        const { data } = event;
-        await prisma.user.update(
-            {
-                where: {
-                    id: data.id,
-                },
-                data: {
-                    email: data?.email_addresses[0]?.email_address,
-                    name: data?.first_name + " " + data?.last_name,
-                    image: data?.image_url,
-                }
-            }
-        )
-    }
-)
+  { id: "update-user-with-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event;
 
-// Create an empty array where we'll export Inngest functions
+    await prisma.user.upsert({
+      where: { id: data.id },
+      update: {
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name ?? ""} ${data?.last_name ?? ""}`,
+        image: data?.image_url,
+      },
+      create: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name ?? ""} ${data?.last_name ?? ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+// Export all functions
 export const functions = [
-    syncUserCreation, 
-    syncUserDeletion, 
-    syncUserUpdation
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
 ];
